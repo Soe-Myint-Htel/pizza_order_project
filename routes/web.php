6e0ac58297f64af8c04213fd50db9abb9c88801f
Soe Myint Htel <gretaoto44@gmail.com>
2022-03-21 22:40:27 -0800

    create admin and user home page with authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,18 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    if(Auth::check()){
+        if(Auth::user()->role == 'admin'){
+            return redirect()->route('admin#index');
+        }else if(Auth::user()->role == 'user'){
+            return redirect()->route('user#index');
+        }
+    }
 })->name('dashboard');
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('/','AdminController@index')->name('admin#index');
+});
+Route::group(['prefix'=>'user'],function(){
+    Route::get('/','UserController@index')->name('user#index');
+});

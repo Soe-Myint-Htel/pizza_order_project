652caf81ef0b94ed99837f8794dbf82a3f86642a
Soe Myint Htel <gretaoto44@gmail.com>
2022-04-13 17:05:18 -0800

    search category csv download

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -9,12 +9,16 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 
 class categoryController extends Controller
 {
     
     public function category(){
+        if(Session::has('CATEGORY_SEARCH')){
+            Session::forget('CATEGORY_SEARCH');
+        }
         $data = Category::select('categories.*',DB::raw('COUNT(pizzas.category_id)as count'))
                 ->leftJoin('pizzas','pizzas.category_id','categories.category_id')
                 ->groupBy('categories.category_id')
@@ -57,13 +61,6 @@ class categoryController extends Controller
         return redirect()->route('admin#category')->with(['successUpdate'=>'Category updated successfully']);
     }
 
-    // search category
-    public function search(Request $request){
-        $data = Category::where('category_name', 'like', '%'.$request->search.'%')->paginate(3);
-        $data->appends($request->all());
-        return view('admin.category.list')->with(['category' => $data]);
-    }
-
     // category item
     public function categoryItem($id){
         $data = Pizza::select('pizzas.*','categories.category_name as categroyName')
@@ -88,16 +85,47 @@ class categoryController extends Controller
         return redirect()->route('admin#category')->with(['successCategory' => 'Category added successfully...']);
     }
 
+    // search category
+    public function search(Request $request){
+        // $data = Category::where('category_name', 'like', '%'.$request->search.'%')->paginate(3);
+
+        $data = Category::select('categories.*',DB::raw('COUNT(pizzas.category_id)as count'))
+                ->leftJoin('pizzas','pizzas.category_id','categories.category_id')
+                ->where('category_name', 'like', '%'.$request->search.'%')  
+                ->groupBy('categories.category_id')
+                ->paginate(3);
+
+        Session::put('CATEGORY_SEARCH', $request->search);
+        
+        $data->appends($request->all());
+        return view('admin.category.list')->with(['category' => $data]);
+    }
+
     //csv download
     //library website link -https://webty.jp/staffblog/production/post-2990/?fbclid=IwAR0EcAZevdQ-wacEQv_p9xoME8IGgQfCHlJ8exhSLjxqwcL1nnIdU-jxV2I
     public function categoryDownload(){
-        $category = Category::get();
+
+        if(Session::has('CATEGORY_SEARCH')){
+            $category = Category::select('categories.*',DB::raw('COUNT(pizzas.category_id)as count'))
+                ->leftJoin('pizzas','pizzas.category_id','categories.category_id')
+                ->where('category_name', 'like', '%'.Session::get('CATEGORY_SEARCH').'%')  
+                ->groupBy('categories.category_id')
+                ->get();
+        }else{
+            $category = Category::select('categories.*',DB::raw('COUNT(pizzas.category_id)as count'))
+                ->leftJoin('pizzas','pizzas.category_id','categories.category_id')
+                ->groupBy('categories.category_id')
+                ->get();
+        }
+
+        
 
         $csvExporter = new \Laracsv\Export();
 
         $csvExporter->build($category, [
             'category_id' => 'ID',
             'category_name' => 'Name',
+            'count' => 'Product Count',
             'created_at' => 'Created Date',
             'updated_at' => 'Updated Date',
         ]);
